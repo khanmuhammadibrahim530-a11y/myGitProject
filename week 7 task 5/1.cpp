2c0e3e04f2e09043e2d06d46da69ff1da710e18d
khanmuhammadibrahim530-a11y <khanmuhammadibrahim530@gmail.com>
2026-01-01 08:45:11 +0500

    Add files via upload

---
+++ week 7 task 5/1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=1;i<=5;i++)
+{for(char j=65;j<=69;j++)
+    cout<<j<<" ";
+cout<<endl;
+}
+return 0;}
---
+++ week 7 task 5/3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{// inverted ight triangle pattern of stars//
+    for (int i = 1; i <= 4; i++)
+    {
+     
+    for(int j=1;j<=4;j++)
+    cout<<i<<j<<"\t";
+    cout<<endl;
+}
+
+return 0;
+}
