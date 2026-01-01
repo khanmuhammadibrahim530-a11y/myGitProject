26e785e26923ec414a42536f2de90c3c472a956a
khanmuhammadibrahim530-a11y <khanmuhammadibrahim530@gmail.com>
2026-01-01 07:50:17 +0500

    Add files via upload

---
+++ question3.cpp
@@ -0,0 +1,13 @@
+/*Q # 3: Write a program that inputs miles from the user and convert miles into kilometres. 
+One mile is equal to 1.069 kilometer.*/  
+#include <iostream>  
+using namespace std;  
+int main(){ 
+    cout<<"Muhammad Ibrahim___72404"<<endl;  
+float miles;     
+cout<<"Enter miles : ";     
+cin>>miles;  
+float kilometer=miles*1.069;    
+cout<<"miles in kilometer ="<<kilometer<<endl;  
+return 0;  
+} 
